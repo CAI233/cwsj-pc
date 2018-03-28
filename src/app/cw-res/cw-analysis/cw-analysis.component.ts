5d5e6a097ca3afbb477b2736567937be409c7788
caitianxu <15250477466@163.com>
2018-03-28 13:56:35 +0800

    save

--- src/app/cw-res/cw-analysis/cw-analysis.component.ts
+++ src/app/cw-res/cw-analysis/cw-analysis.component.ts
@@ -1,15 +1,91 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AppService } from '../../app.service';
 @Component({
   selector: 'app-cw-analysis',
   templateUrl: './cw-analysis.component.html',
   styleUrls: ['./cw-analysis.component.css']
 })
 export class CwAnalysisComponent implements OnInit {
-
-  constructor() { }
+  param: object ={
+    total: 0,
+    pageSize: 10,
+    pageNum: 1,
+    pages: 0
+  }
+  _loading: boolean = false;
+  isVisibleMiddle: boolean = false;
+  tableData: any = [];
+  formBean: any = {
+    task_id: null,
+    task_name: null,
+    task_url: null
+  }
+  myForm: any;
+  constructor(public service: AppService) { }
 
   ngOnInit() {
+    this.myForm = this.service.fb.group({
+      task_name: [null, [this.service.validators.required]],
+      task_url: [null, [this.service.validators.required]]
+    });
+    this._reload();
   }
+  //创建任务
+  _cwAnalysisAdd(){
+    this.isVisibleMiddle = true;
 
+  }
+  //提交任务
+  _submitForm(){
+    for (const i in this.myForm.controls) {
+      this.myForm.controls[i].markAsDirty();
+    }
+    if(!this.myForm.valid){
+      return false;
+    }
+    console.log(this.formBean)
+  }
+  //取消编辑
+  _cancelMiddle(){
+    this.isVisibleMiddle = false;
+  }
+  //提交编辑
+  _okMiddle(){
+    this._submitForm();
+  }
+  //刷新列表
+  _reload(event?: any){
+    console.log(this.param)
+    this._loading = true;
+    this.tableData = [{
+      task_name: '第一批内容',
+      task_url: '/2018/03/28',
+      create_time: '2018-03-28',
+      state: 1, //1 未解析  2解析中  3解析完成 4解析失败
+      success_num: 0,
+      error_num: 0
+    },{
+      task_name: '第一批内容',
+      task_url: '/2018/03/28',
+      create_time: '2018-03-28',
+      state: 2, //1 未解析  2解析中  3解析完成 4解析失败
+      success_num: 18988,
+      error_num: 7
+    },{
+      task_name: '第一批内容',
+      task_url: '/2018/03/28',
+      create_time: '2018-03-28',
+      state: 3, //1 未解析  2解析中  3解析完成 4解析失败
+      success_num: 18988,
+      error_num: 7
+    },{
+      task_name: '第一批内容',
+      task_url: '/2018/03/28',
+      create_time: '2018-03-28',
+      state: 4, //1 未解析  2解析中  3解析完成 4解析失败
+      success_num: 0,
+      error_num: 0
+    }];
+    this._loading = false;
+  }
 }
--- src/app/cw-res/cw-res-class/cw-res-class.component.ts
+++ src/app/cw-res/cw-res-class/cw-res-class.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-cw-res-class',
@@ -6,10 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cw-res-class.component.css']
 })
 export class CwResClassComponent implements OnInit {
-
-  constructor() { }
+  param: any = {
+    key: null
+  }
+  dataSet = [];
+  expandDataCache = {};
+  constructor(public service: AppService) { }
 
   ngOnInit() {
+    this._reload();
+    this.dataSet.forEach(item => {
+      this.expandDataCache[ item.key ] = this.convertTreeToList(item);
+    });
   }
 
+  //////////////////tree转化
+  collapse(array, data, $event) {
+    if ($event === false) {
+      if (data.children) {
+        data.children.forEach(d => {
+          const target = array.find(a => a.key === d.key);
+          target.expand = false;
+          this.collapse(array, target, false);
+        });
+      } else {
+        return;
+      }
+    }
+  }
+  convertTreeToList(root) {
+    const stack = [], array = [], hashMap = {};
+    stack.push({ ...root, level: 0, expand: false });
+
+    while (stack.length !== 0) {
+      const node = stack.pop();
+      this.visitNode(node, hashMap, array);
+      if (node.children) {
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          stack.push({ ...node.children[ i ], level: node.level + 1, expand: false, parent: node });
+        }
+      }
+    }
+
+    return array;
+  }
+  visitNode(node, hashMap, array) {
+    if (!hashMap[ node.key ]) {
+      hashMap[ node.key ] = true;
+      array.push(node);
+    }
+  }
+  //////////////////tree转化
+  //获取资源分类
+  _reload(){
+    this.service.post('/api/busiz/cat/list',{
+      searchText: null
+    }).then(success => {
+      console.log(success)
+    })
+  }
 }
