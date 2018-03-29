4e21040f30d83a703e11bc068f7fa34ebe2e73d2
熊健成 <muaxiaobaobei@qq.com>
2018-03-29 18:19:24 +0800

    作品标签(已对接) 作品分类(已对接) 作品列表(未对接)

--- src/app/cw-works/cw-works-list/cw-works-list.component.ts
+++ src/app/cw-works/cw-works-list/cw-works-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AppService } from '../../app.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cw-works-list',
@@ -6,10 +8,269 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cw-works-list.component.css']
 })
 export class CwWorksListComponent implements OnInit {
-
-  constructor() { }
-
+  @ViewChild("nzTabAdd") nzTabAdd;
+  _allChecked = false;
+  _indeterminate = false;
+  tableData: any = []; //数据列表
+  param: any = {
+    org_id: null,
+    searchText: null,
+    total: 0,
+    pageSize: 10,
+    pageNum: 1
+  };
+  paramCol: any = {
+    dept_id: null,
+    state: null,
+    date: null,
+    searchText: null
+  }
+  sortMap = {
+    org_name: null,
+    org_code: null
+  };
+  isCollapse: any = true;
+  _loading: boolean = true;
+  //省 市 区 街 
+  _address: any;
+  //ckeditor配置
+  config: any = {
+    width: '100%',
+    toolbar: 'MyToolbar',
+    toolbar_MyToolbar:
+      [
+        { name: 'clipboard', items: ['Undo', 'Redo', '-'] },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+        { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar'] },
+        { name: 'tools', items: ['Maximize'] },
+        { name: 'document', items: ['Source'] },
+        { name: 'basicstyles', items: ['Bold', 'Italic', 'Strike', 'RemoveFormat', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-'] },
+        { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Algin', 'Outdent', 'Indent'] },
+        { name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
+        { name: 'colors', items: ['TextColor', 'BGColor'] },
+      ],
+    filebrowserImageUploadUrl: this.service.ctxPath + '/api/system/file/upload',
+  }
+  // 实例化一个对象
+  constructor(public service: AppService) { }
+  //表单
+  myForm: FormGroup;
+  formTitle: string;
+  isVisibleMiddle: boolean = false;
+  selectedIndex: number = 0;
+  formBean: any = {
+    role_id: null,
+    role_idss: null,
+    email: null,
+    phone: null,
+    vote_role: null,
+  };
   ngOnInit() {
+    this.reload();
+    this.myForm = this.service.fb.group({
+      org_name: [null, [this.service.validators.required]],
+      org_code: [null, [this.service.validators.required]],
+      streetParent: [null, [this.service.validators.required]],
+      office_address: [null, [this.service.validators.required]],
+      link_man: [null, [this.service.validators.required]],
+      link_mobile: [null, [this.service.validators.required]],
+      auth_date_begin: [null, [this.service.validators.required]],
+      auth_date_end: [null, [this.service.validators.required]],
+      remark: [false],
+      time: [false],
+      phone: [false],
+      works_name: [false],
+      vote_role: [false],
+      email: [false],
+      role_id: [false]
+    })
+  }
+
+  loadData(e: { option: any, index: number, resolve: Function, reject: Function }): void {
+    if (e.index === -1) {
+      this.service.post('/api/system/region/list/tree', {
+        code: null
+      }).then(success => {
+        e.resolve(success.data);
+      })
+      return;
+    }
+    const option = e.option;
+    option.loading = true;
+    this.service.post('/api/system/region/list/tree', {
+      code: option.code
+    }).then(success => {
+      option.loading = false;
+      if (e.index == 2)
+        success.data.forEach(element => element.isLeaf = true);
+      e.resolve(success.data);
+    })
+  }
+  //打开
+  showModalMiddle(bean?: any) {
+    this.formBean = {};
+    if (bean) {
+      for (let i in bean) {
+        this.formBean[i] = bean[i];
+      }
+      this.formTitle = "修改作品";
+    }
+    else {
+      this.formTitle = "新增作品";
+    }
+    this.isVisibleMiddle = true;
+    this.selectedIndex = 1;
+  };
+
+  //关闭
+  handleCancelMiddle($event) {
+    this.isVisibleMiddle = false;
+    this.formClear()
+  }
+  //确定
+  handleOkMiddle($event) {
+    this._submitForm();
+  }
+  formClear() {
+    this.myForm.reset();
+  }
+
+  //提交
+  _submitForm() {
+    for (const i in this.myForm.controls) {
+      this.myForm.controls[i].markAsDirty();
+    }
+    if (this.myForm.valid) {
+      this.service.post('//api/busiz/video/save', this.formBean).then(success => {
+        if (success.code == 0) {
+          this.isVisibleMiddle = false;
+          this.formClear()
+          this.reload();
+        }
+        else {
+          this.service.message.error(success.message);
+        }
+      })
+    }
+  }
+  //修改
+  editModalMiddle() {
+    if (this.tableData.filter(value => value.checked).length != 1) {
+      this.service.message.warning('请选择修改数据，并且同时只能修改一条!');
+    }
+    else {
+      let bean = this.tableData.filter(value => value.checked)[0];
+      for (let i in bean) {
+        this.formBean[i] = bean[i];
+      }
+      //地理组织
+      this.formBean.streetParent = [{
+        code: bean.province_code,
+        region_name: bean.province
+      }, {
+        code: bean.city_code,
+        region_name: bean.city
+      }, {
+        code: bean.area_code,
+        region_name: bean.area
+      }, {
+        code: bean.street_code,
+        region_name: bean.street
+      }];
+      console.log(this.formBean)
+      this.formTitle = '修改机构';
+      this.isVisibleMiddle = true;
+    }
+  }
+  //删除
+  delRows() {
+    if (this.tableData.filter(value => value.checked).length < 1) {
+      this.service.message.warning('你没有选择需要删除的数据内容!');
+    }
+    else {
+      let ids = [];
+      this.tableData.filter(value => value.checked).forEach(item => { ids.push(item.org_id) })
+      this.service.post('/api/busiz/video/delete', {
+        ids: ids, mark: 'del'
+      }).then(success => {
+        if (success.code == 0) {
+          this.reload();
+        }
+        else {
+          this.service.message.error(success.message);
+        }
+      })
+    }
+  }
+  //重新查询
+  reload(reset?: any) {
+    if (reset == true) {
+      this.param.pageNum = 1;
+    }
+    this._loading = true;
+    this.service.post('/api/busiz/works/list', this.param).then(success => {
+      this._loading = false;
+      if (success.code == 0) {
+        this.tableData = success.data.rows;
+        this.param.total = success.data.total;
+      }
+      else {
+        this.tableData = [];
+        this.param.total = 0;
+        this.service.message.error(success.message);
+      }
+    })
+  }
+  //全选
+  _checkAll(value) {
+    if (value) {
+      this.tableData.forEach(data => {
+        if (!data.disabled) {
+          data.checked = true;
+        }
+      });
+    } else {
+      this.tableData.forEach(data => data.checked = false);
+    }
+    this._refreshStatus();
+  }
+  _refreshStatus() {
+    const allChecked = this.tableData.every(value => value.disabled || value.checked);
+    const allUnChecked = this.tableData.every(value => value.disabled || !value.checked);
+    this._allChecked = allChecked;
+    this._indeterminate = (!allChecked) && (!allUnChecked);
+  }
+  //排序
+  sort(name, value) {
+    if (value) {
+      this.param.sort_name = name;
+      this.param.sort_rule = value == 'ascend' ? 'asc' : 'desc';
+    }
+    else {
+      this.param.sort_name = null;
+      this.param.sort_rule = null;
+    }
+    Object.keys(this.sortMap).forEach(key => {
+      if (key !== name) {
+        this.sortMap[key] = null;
+      } else {
+        this.sortMap[key] = value;
+      }
+    });
+    this.reload();
+  }
+
+  //状态
+  _enabled(data) {
+    if (this.service.validataAction('cw_train_list_enable')) {
+      data.enabled = data.enabled == 1 ? 2 : 1;
+      this.service.post('/api/system/organization/setEnabled', {
+        ids: [data.org_id],
+        enabled: data.enabled
+      }).then(success => {
+        this.reload();
+      })
+    }
   }
 
 }
