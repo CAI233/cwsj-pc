4599d65039d67eaa58d422b96c619ed9cd173d70
caitianxu <15250477466@163.com>
2018-03-31 19:01:12 +0800

    save

--- src/app/cw-works/cw-works.module.ts
+++ src/app/cw-works/cw-works.module.ts
@@ -9,21 +9,13 @@ import { NgZorroAntdModule, NZ_MESSAGE_CONFIG, NZ_NOTIFICATION_CONFIG } from 'ng
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";//引入NZ 使用的form核心
 import { routes } from './cw-works.routing';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
-//富文本编辑器
-=======
->>>>>>> 4be400b5ec0e388003dd754a0735d8acbb05762f
 @NgModule({
   imports: [
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes),
     NgZorroAntdModule.forRoot(), //引入NZ
-<<<<<<< HEAD
-    CommonModule
-=======
     CommonModule,
->>>>>>> 4be400b5ec0e388003dd754a0735d8acbb05762f
   ],
   declarations: [CwWorksClassComponent, CwWorksTagComponent, CwWorksListComponent],
   providers: [  //引入NZ
