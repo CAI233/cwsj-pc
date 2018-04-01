ee44ed495bcac14d4453e24acc9191cc81e447bd
熊健成 <muaxiaobaobei@qq.com>
2018-04-01 18:55:12 +0800

    作品管理, ---教育表格

--- src/app/cw-works/cw-works-list/cw-works-list.component.ts
+++ src/app/cw-works/cw-works-list/cw-works-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppService } from '../../app.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 
 declare let wangEditor: any
@@ -85,19 +85,6 @@ export class CwWorksListComponent implements OnInit {
       works_remark: false,
     })
   }
-  _initEditor() {
-    setTimeout(time => {
-      var editor = new wangEditor('#editor');
-      editor.customConfig.uploadImgShowBase64 = true;
-      editor.create();
-      editor.txt.html(this.formBean.works_remark);
-      // editor.txt.clear();
-      // editor.txt.append('<p>追加的内容</p>');
-      // console.log(editor.txt.html())
-      // console.log(editor.txt.text())
-    })
-  }
-
   loadCat(e: { option: any, index: number, resolve: Function, reject: Function }): void {
     if (e.index === -1) {
       this.service.post('/api/busiz/works/cat/list', {
@@ -126,6 +113,7 @@ export class CwWorksListComponent implements OnInit {
           this.formBean.works_cat_idss.push({ cat_id: element, cat_name: this.formBean.works_cat_names.split(',')[index] })
         });
       }
+      this.formBean.tag_ids = this.formBean.tag_ids.split(',').map(element => parseInt(element))
       this.formTitle = "修改作品";
     }
     else {
@@ -140,9 +128,54 @@ export class CwWorksListComponent implements OnInit {
     }
     this.isVisibleMiddle = true;
     this.selectedIndex = 1;
-    this._initEditor();
-  };
+    if (this.formBean.works_type == "资源包") {
+      this.resourceArray = [];
+      this.myForm1 = this.service.fb.group({});
+      this.service.post('/api/busiz/works/resources/list', { works_id: this.formBean.works_id }).then(success => {
+        if (success.data && success.data.length > 0) {
+          this.resourceArray = success.data
+          this.resourceArray.forEach((element, index) => {
+            element.order_id = index
+            element.resources_name = `resources${index}`;
+            this.myForm1.addControl(element.resources_name, new FormControl(null, this.service.validators.required));
+          })
+        } else {
+          this.addField1()
+        }
+      })
+    }
+    if (this.formBean.works_type == "教育表格") {
+      this.questionArray = [];
+      this.myForm4 = this.service.fb.group({});
+      this.service.post('/api/busiz/works/form/list', { works_id: this.formBean.works_id }).then(success => {
+        if (success.data && success.data.length > 0) {
+          this.questionArray = success.data
+          this.questionArray.forEach((element, index) => {
+            element.order_id = index
+            element.question_name = `question${index}`;
+            element.answer_name = `answer${index}`;
+            this.myForm4.addControl(element.question_name, new FormControl(null, this.service.validators.required));
+            this.myForm4.addControl(element.answer_name, new FormControl(false));
+            this.myForm4.addControl(element.resources_id, new FormControl(false));
 
+          })
+        } else {
+          this.addField4()
+        }
+      })
+    }
+
+    //获取资源下拉
+    this.service.post('/api/busiz/res/getlist', {
+      pageNum: 1,
+      pageSize: 10000
+    }).then(success => {
+      if (success.code == 0) {
+        this.resourceList = success.data.rows
+      }
+    })
+  };
+  resourceList: any = [];
   //关闭
   handleCancelMiddle($event) {
     this.isVisibleMiddle = false;
@@ -179,13 +212,22 @@ export class CwWorksListComponent implements OnInit {
       //   this.service.message.error('请上传封面');
       //   return false;
       // }
-
-      this.formBean.works_cat_id = this.formBean.works_cat_idss[this.formBean.works_cat_idss.length - 1]
+      this.formBean.works_cat_ids_array = []
+      this.formBean.works_cat_idss.forEach(element => {
+        if (typeof (element) == 'object') {
+          this.formBean.works_cat_ids_array.push(element.cat_id);
+        }
+        else {
+          this.formBean.works_cat_ids_array.push(element);
+        }
+      });
+      this.formBean.works_cat_id = this.formBean.works_cat_ids_array[this.formBean.works_cat_ids_array.length - 1];
+      this.formBean.works_cat_ids = this.formBean.works_cat_ids_array.join(',');
       this.formBean.works_cat_names = this.cat_idss._displayLabelContext.labels.join(',');
+
       this.service.post('/api/busiz/works/save', this.formBean).then(success => {
         if (success.code == 0) {
-          this.isVisibleMiddle = false;
-          this.formClear()
+          this.service.message.success('保存成功')
           this.reload();
         }
         else {
@@ -285,10 +327,17 @@ export class CwWorksListComponent implements OnInit {
   //上架下架
   statusWorks(data) {
     if (this.service.validataAction('cw_works_list_put')) {
-      data.status = data.status == 1 ? 2 : 1;
-      this.service.post('/api/busiz/works/save', data).then(success => {
+      if (data.audit_status == 3) {
+        data.status = data.status == 1 ? 2 : 1;
+        data.tag_ids = data.tag_ids.split(',').map(element => parseInt(element))
+        this.service.post('/api/busiz/works/save', data).then(success => {
+          this.reload();
+        })
+      }
+      else {
+        this.service.message.warning('审核未通过作品无法上架!');
         this.reload();
-      })
+      }
     }
   }
   //提交审核
@@ -311,5 +360,162 @@ export class CwWorksListComponent implements OnInit {
       })
     }
   }
+  //通过驳回
+  auditStatus(data, status) {
+    this.service.post('/api/busiz/works/audit', { ids: [data.works_id], audit_status: status }).then(success => {
+      if (success.code == 0) {
+        this.reload();
+      } else {
+        this.service.message.error(success.message);
+      }
+    })
+  }
+  //详情
+  worksDetail() {
+    window.open('www.baidu.com')
+  }
 
+
+
+
+
+  //---------------------------------------------  资源文件 start --------------------------------------------
+  myForm1: FormGroup;
+  resourceArray: any = [];
+  addField1(e?: MouseEvent) {
+    if (e) {
+      e.preventDefault();
+    }
+    const order_id = (this.resourceArray.length > 0) ? this.resourceArray[this.resourceArray.length - 1].order_id + 1 : 0;
+    const control = {
+      id: null,
+      order_id,
+      resources_name: `resources${order_id}`,
+      resources_id: null,
+    };
+    const index = this.resourceArray.push(control);
+    this.myForm1.addControl(this.resourceArray[index - 1].resources_name, new FormControl(null, this.service.validators.required));
+  }
+
+  removeField1(i, e: MouseEvent) {
+    e.preventDefault();
+    if (this.resourceArray.length > 1) {
+      if (i.id) {
+        this.service.post('/api/busiz/works/form/del', { id: i.id }).then(success => {
+          if (success.code == 0) {
+            const index = this.resourceArray.indexOf(i);
+            this.resourceArray.splice(index, 1);
+            this.myForm1.removeControl(i.resources_name);
+            this.service.message.success("删除成功");
+          } else {
+            this.service.message.error(success.message);
+          }
+        })
+      } else {
+        const index = this.resourceArray.indexOf(i);
+        this.resourceArray.splice(index, 1);
+        this.myForm1.removeControl(i.resources_name);
+      }
+    }
+  }
+  getFormControl1(name) {
+    return this.myForm1.controls[name];
+  }
+  submitForm1(data?: any, i?: number) {
+    if (!this.formBean.works_id) {
+      this.service.message.warning('请先保存作品基本信息');
+      return false;
+    }
+    this.myForm1.controls[i].markAsDirty();
+    this.service.post('/api/busiz/works/form/save', {
+      works_id: this.formBean.works_id,
+      id: data.id,
+      question: data.question,
+      answer: data.answer,
+      resources_id: data.resources_id,
+    }).then(success => {
+      if (success.code == 0) {
+        this.service.message.success("保存成功")
+      } else {
+        this.service.message.error(success.message)
+      }
+    })
+  }
+
+  //---------------------------------------------  资源文件 end --------------------------------------------
+  //---------------------------------------------  教育表格 start --------------------------------------------
+  myForm4: FormGroup;
+  questionArray: any = [];
+  addField4(e?: MouseEvent) {
+    if (e) {
+      e.preventDefault();
+    }
+    const order_id = (this.questionArray.length > 0) ? this.questionArray[this.questionArray.length - 1].order_id + 1 : 0;
+    const control = {
+      id: null,
+      order_id,
+      question_name: `question${order_id}`,
+      answer_name: `answer${order_id}`,
+      answer: null,
+      question: null,
+      resources_id: null,
+      res_name: null,
+      resource_path: null
+    };
+    const index = this.questionArray.push(control);
+    this.myForm4.addControl(this.questionArray[index - 1].question_name, new FormControl(null, this.service.validators.required));
+    this.myForm4.addControl(this.questionArray[index - 1].answer_name, new FormControl(false));
+    this.myForm4.addControl(this.questionArray[index - 1].resources_id, new FormControl(false));
+  }
+
+  removeField4(i, e: MouseEvent) {
+    e.preventDefault();
+    if (this.questionArray.length > 1) {
+      if (i.id) {
+        this.service.post('/api/busiz/works/form/del', { id: i.id }).then(success => {
+          if (success.code == 0) {
+            const index = this.questionArray.indexOf(i);
+            this.questionArray.splice(index, 1);
+            this.myForm4.removeControl(i.question_name);
+            this.myForm4.removeControl(i.answer_name);
+            this.myForm4.removeControl(i.resources_id);
+            this.service.message.success("删除成功");
+          } else {
+            this.service.message.error(success.message);
+          }
+        })
+      } else {
+        const index = this.questionArray.indexOf(i);
+        this.questionArray.splice(index, 1);
+        this.myForm4.removeControl(i.question_name);
+        this.myForm4.removeControl(i.answer_name);
+        this.myForm4.removeControl(i.resources_id);
+      }
+    }
+  }
+  getFormControl4(name) {
+    return this.myForm4.controls[name];
+  }
+  submitForm4(data?: any, i?: number) {
+    if (!this.formBean.works_id) {
+      this.service.message.warning('请先保存作品基本信息');
+      return false;
+    }
+    this.myForm4.controls[i].markAsDirty();
+    this.service.post('/api/busiz/works/form/save', {
+      works_id: this.formBean.works_id,
+      id: data.id,
+      question: data.question,
+      answer: data.answer,
+      resources_id: data.resources_id,
+    }).then(success => {
+      if (success.code == 0) {
+        this.service.message.success("保存成功")
+      } else {
+        this.service.message.error(success.message)
+      }
+    })
+  }
+
+  //---------------------------------------------  教育表格 end --------------------------------------------
 }
