efacd739e2414dded2aa98c477a761048ac7ff21
CAI233 <495397430@qq.com>
2018-05-15 15:11:00 +0800

    5.15 修改

--- src/app/cw-stat/cw-market/cw-market.component.ts
+++ src/app/cw-stat/cw-market/cw-market.component.ts
@@ -1,10 +1,7 @@
 
-import 'zone.js';
-import 'reflect-metadata';
+
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app.service';
-import { ViserModule } from 'viser-ng';
-// import { DataSet } from '@antv/data-set';
 
 // const data = [
 //   { year: '1991', value: 3 },
@@ -56,26 +53,39 @@ export class CwMarketComponent implements OnInit {
     this.service.post('/api/busiz/statistical/first/days',this.param).then(success => {
       this._loading = false;
       if(success.code==0){
-        // this.data = success.data;
-        this.data = [
-          { data: '1991-12-1', value: 3 },
-          { data: '1992-12-1', value: 4 },
-          { data: '1993-12-1', value: 3.5 },
-          { data: '1994-12-1', value: 5 },
-          { data: '1995-12-1', value: 4.9 },
-          { data: '1996-12-1', value: 6 },
-          { data: '1997-12-1', value: 7 },
-          { data: '1998-12-1', value: 9 },
-          { data: '1999-12-1', value: 13 },
-        ];
+        let nowArr = [];
+        success.data.forEach(element => {
+          nowArr.push({
+            data:element.date,
+            money:element.money || 0 
+          })
+        });
+        // console.log(nowArr)
+
+        this.data = nowArr;
+        // console.log(this.nowData )
+
+        // this.data = [
+        //   { data: '1991-12-1', money: 3 },
+        //   { data: '1992-12-1', money: 4 },
+        //   { data: '1993-12-1', money: 3.5 },
+        //   { data: '1994-12-1', money: 5 },
+        //   { data: '1995-12-1', money: 4.9 },
+        //   { data: '1996-12-1', money: 6 },
+        //   { data: '1997-12-1', money: 7 },
+        //   { data: '1998-12-1', money: 9 },
+        //   { data: '1999-12-1', money: 13 },
+        // ];
 
         this.scale = [{
-          dataKey: 'value',
+          dataKey: 'money',
           min: 0,
+          alias:'金额'
         },{
-          dataKey: 'data',
+          dataKey: 'month',
           min: 0,
           max: 1,
+          
         }];
         
       }else{
@@ -113,221 +123,4 @@ export class CwMarketComponent implements OnInit {
     // 加载销售统计数据
     this.get_load();
   }
-
-  // [{
-  //   "cumulative": null,
-  //   "year": "2018-05-10",
-  //   "first_days": null,
-  //   "value": 12,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-09",
-  //   "first_days": null,
-  //   "value": 24,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-08",
-  //   "first_days": null,
-  //   "value": 32,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-07",
-  //   "first_days": null,
-  //   "value": 13.0,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-06",
-  //   "first_days": null,
-  //   "value": 6.01,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-05",
-  //   "first_days": null,
-  //   "value": 234,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-04",
-  //   "first_days": null,
-  //   "value": 21,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-03",
-  //   "first_days": null,
-  //   "value": 45,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-02",
-  //   "first_days": null,
-  //   "value": 34,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-05-01",
-  //   "first_days": null,
-  //   "value": 65,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-30",
-  //   "first_days": null,
-  //   "value": 5,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-29",
-  //   "first_days": null,
-  //   "value": 234,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-28",
-  //   "first_days": null,
-  //   "value": 67,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-27",
-  //   "first_days": null,
-  //   "value": 4,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-26",
-  //   "first_days": null,
-  //   "value": 5,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-25",
-  //   "first_days": null,
-  //   "value": 3,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-24",
-  //   "first_days": null,
-  //   "value": 23,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-23",
-  //   "first_days": null,
-  //   "value": 45.02,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-22",
-  //   "first_days": null,
-  //   "value": 66,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-21",
-  //   "first_days": null,
-  //   "value": 23,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-20",
-  //   "first_days": null,
-  //   "value": 43,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-19",
-  //   "first_days": null,
-  //   "value": 43,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-18",
-  //   "first_days": null,
-  //   "value": 23,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-17",
-  //   "first_days": null,
-  //   "value": 21,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-16",
-  //   "first_days": null,
-  //   "value": 11,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-15",
-  //   "first_days": null,
-  //   "value": 2,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-14",
-  //   "first_days": null,
-  //   "value": 22,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-13",
-  //   "first_days": null,
-  //   "value": 23,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-12",
-  //   "first_days": null,
-  //   "value": 1,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }, {
-  //   "cumulative": null,
-  //   "year": "2018-04-11",
-  //   "first_days": null,
-  //   "value": 100,
-  //   "the_days": null,
-  //   "yesterday": null
-  // }]
-
-
-  
-
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
+import { DataSet } from '@antv/data-set';
+
 
 
 // declare let wangEditor: any
@@ -9,13 +11,87 @@ import { AppService } from '../app.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+
+  data : any = [];
+  scale : any = [];
+  tooltip : any = [];
+  itemTpl:any = '';//鼠标滑过提示
+  legendTpl : any = '';//图表下方展示信息
+  dv : any = null;
+
+  forceFit: boolean = true;
+  height: number = 400;
+  pieStyle = {
+    stroke: "#fff",
+    lineWidth: 2
+  };
+  labelConfig = ['percent', {
+    offset: -40,
+    textStyle: {
+      rotate: 0,
+      textAlign: 'center',
+      shadowBlur: 2,
+      shadowColor: 'rgba(0, 0, 0, .45)'
+    }
+  }];
   constructor(public service: AppService) { }
 
   ngOnInit() {
    
-  }
-  // ngAfterViewInit(){
+    let ds = new DataSet();
+   
+    let sourceData = [
+      { item: '语文', count: 40 },
+      { item: '数学', count: 21 },
+      { item: '儿童文学', count: 17 }
+    ];
+    this.scale = [{
+      dataKey: 'percent',
+      min: 0,
+      formatter: '.0%',
+    }];
+    // let color = ['name', ['#BAE7FF', '#7FC9FE', '#71E3E3', '#ABF5F5', '#8EE0A1', '#BAF5C4']];
+    this.itemTpl = '<li><span ></span>{title}: {value}</li>';
+    this.tooltip = [
+      'item*percent',(item, percent) => {
+        percent = (percent * 100).toFixed(2) + '%';
+        return {
+          title: item,
+          value: percent,
+        }
+      }]
+
+      
+    this.legendTpl = ``;
+
+
+    // let dv = ds.createView().source(sourceData);
     
+    this.dv = ds.createView().source(sourceData);
+    
+    this.dv.transform({
+      tooltip:false,
+      showTitle: false,
+      type: 'percent',
+      field: 'count',
+      dimension: 'item',
+      as: 'percent'
+    });
+
+  
+    console.log(this.dv)
+    this.load();
+  }
+
+// 加载数据
+  load(){
+
+
+    this.data  = this.dv.rows;
+  }
+
+  // ngAfterViewInit(){
   //   var editor = new wangEditor('#editor');
   //   editor.customConfig.uploadImgShowBase64 = true;
   //   editor.create();
