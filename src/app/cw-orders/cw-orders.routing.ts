4a5737374053a11696d0a63fdb19e065aa7c2d64
熊健成 <muaxiaobaobei@qq.com>
2018-05-17 15:58:23 +0800

    结构调整

--- src/app/cw-orders/cw-orders.routing.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-//模块组件
-import { CwOrdersListComponent } from './cw-orders-list/cw-orders-list.component';
-
-export const routes: Routes = [
-  { path: 'cw_orders_list', component: CwOrdersListComponent, data: { title: '订单管理', module: 'cw_orders_list', power: "SHOW" } },
-];
