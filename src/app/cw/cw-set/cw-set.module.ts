78a4d4ce410c0f0108d9e04cfdbb4fff1fb83025
CAI233 <495397430@qq.com>
2019-06-05 13:42:44 +0800

    ng-zorro ^0.6.15

--- src/app/cw/cw-set/cw-set.module.ts
+++ src/app/cw/cw-set/cw-set.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CwWinComponent } from './cw-win/cw-win.component';
-import { CwAdvListComponent } from './cw-adv-list/cw-adv-list.component';
-import { CwAdvClassComponent } from './cw-adv-class/cw-adv-class.component';
+// import { CwAdvListComponent } from './cw-adv-list/cw-adv-list.component';
+// import { CwAdvClassComponent } from './cw-adv-class/cw-adv-class.component';
 import { CwLogisComponent } from './cw-logis/cw-logis.component';
-import { CwEmailConComponent } from './cw-email-con/cw-email-con.component';
-import { CwEmailModuleComponent } from './cw-email-module/cw-email-module.component';
-import { CwEmailSendComponent } from './cw-email-send/cw-email-send.component';
+// import { CwEmailConComponent } from './cw-email-con/cw-email-con.component';
+// import { CwEmailModuleComponent } from './cw-email-module/cw-email-module.component';
+// import { CwEmailSendComponent } from './cw-email-send/cw-email-send.component';
 
 //web ui 
 import { NgZorroAntdModule, NZ_MESSAGE_CONFIG, NZ_NOTIFICATION_CONFIG } from 'ng-zorro-antd';//引入NZ 使用的form核心
@@ -23,7 +23,8 @@ import { RouterModule } from '@angular/router';
     NgZorroAntdModule.forRoot(), //引入NZ
     CommonModule
   ],
-  declarations: [CwWinComponent, CwEmailConComponent, CwEmailModuleComponent, CwEmailSendComponent, CwAdvListComponent,CwAdvClassComponent, CwLogisComponent],
+  // declarations: [CwWinComponent, CwEmailConComponent, CwEmailModuleComponent, CwEmailSendComponent, CwAdvListComponent,CwAdvClassComponent, CwLogisComponent],
+  declarations: [CwWinComponent, CwLogisComponent],
   providers: [  //引入NZ
     { provide: NZ_MESSAGE_CONFIG, useValue: { nzDuration: 3000 } },
     { provide: NZ_NOTIFICATION_CONFIG, useValue: { nzTop: '20px' } }
